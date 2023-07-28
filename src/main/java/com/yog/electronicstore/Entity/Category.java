1e3b0f19fe31cfe7c42a2bb898d0b03fbd8fe38f
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 17:11:51 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Entity/Category.java
@@ -0,0 +1,33 @@
+package com.yog.electronicstore.Entity;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="categories")
+@Builder
+public class Category {
+    @Id
+    @Column(name="id")
+    private String CategoryId;
+    @Column(name="category_title", length = 60,nullable = false)
+    private String title;
+    @Column(name="category_desc", length = 500)
+    private String description;
+    private String coverImage;
+
+
+    //Other attributes
+
+
+}
+
