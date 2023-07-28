3506847e75a0978a2a159ec4dc97e8976b766b5e
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 17:20:19 +0530

    new commit

--- src/main/java/com/yog/electronicstore/Entity/OrderItem.java
+++ src/main/java/com/yog/electronicstore/Entity/OrderItem.java
@@ -1,4 +1,37 @@
 package com.yog.electronicstore.Entity;
 
-public class OrderItem {
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name="orderItems")
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderItemId;
+
+    @Column(name="orderItem_quantity")
+    private int  quantity;
+
+    @Column(name="orderItem_totalPrize")
+    private int totalPrize;
+
+    //order -orderItem mapping
+    @ManyToOne
+    @JoinColumn(name="order_Id")
+    private Order order;
+
+    //orderItem -product mapping
+    @OneToOne
+    @JoinColumn(name="product_Id")
+    private Product product;
+
 }
+
