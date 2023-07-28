1fcd469a0abb8519c68de2fae058981ad30d4089
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 17:36:40 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Repository/OrderRepo.java
@@ -0,0 +1,22 @@
+/**
+package com.yog.electronicstore.Repository;
+
+import com.rinku.electronic.store.ElectronicStore.entity.Order;
+import com.rinku.electronic.store.ElectronicStore.entity.User;
+import org.hibernate.internal.CriteriaImpl;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class OrderRepo extends JpaRepository<Order,String> {
+
+    List<Order> findByUser(User user);
+}
+**/
