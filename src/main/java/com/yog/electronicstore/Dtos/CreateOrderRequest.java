54ebbf0c9ca81740036b65e42ea2fc332bc6264e
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 19:16:37 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Dtos/CreateOrderRequest.java
@@ -0,0 +1,29 @@
+package com.yog.electronicstore.Dtos;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateOrderRequest {
+
+    private String userId;
+    private String cartId;
+    private String orderStatus="PENDING";
+    private String paymentStatus="NOTPAID";
+    private int orderAmount;
+    @NotBlank(message = "Billing Address is required !!")
+    private String billingAddress;
+    @NotBlank(message = "Billing Phone is required !!")
+    private String billingPhone;
+    @NotBlank(message = "Billing Name is required !!")
+    private String billingName;
+
+}
