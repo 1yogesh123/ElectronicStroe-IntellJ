762a65a6bd26a6a20153dda6e8ff0711ba4a20d7
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 19:16:18 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Dtos/CategoryDto.java
@@ -0,0 +1,26 @@
+package com.yog.electronicstore.Dtos;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDto {
+    private String CategoryId;
+    @NotBlank
+    @Size(min = 5, max = 15, message = "title must be within limit (min =5,max=15) .....!! ")
+    private String title;
+    @NotBlank
+    //  @Size(min = 5, max = 30, message = "description must be within min length 5 and max length 30")
+    private String description;
+    private String coverImage;
+
+
+}
