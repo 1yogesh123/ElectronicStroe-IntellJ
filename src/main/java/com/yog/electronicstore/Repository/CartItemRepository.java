35a75091485fe5ab401f774352b569c353b73208
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 17:35:42 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Repository/CartItemRepository.java
@@ -0,0 +1,11 @@
+package com.yog.electronicstore.Repository;
+
+
+import com.yog.electronicstore.Entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
+
+}
