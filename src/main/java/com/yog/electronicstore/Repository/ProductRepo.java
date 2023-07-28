27fa888c2209785b8820968e726325443570870f
1yogesh123 <yogshelar333@gmail.com>
2023-07-28 19:18:46 +0530

    new commit

--- src/main/java/com/yog/electronicstore/Repository/ProductRepo.java
+++ src/main/java/com/yog/electronicstore/Repository/ProductRepo.java
@@ -1,4 +1,14 @@
 package com.yog.electronicstore.Repository;
 
-public interface ProductRepo {
+import com.yog.electronicstore.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product,String> {
+    Page<Product> findByTitleContaining(String subTitle,Pageable pageable);
+    Page<Product> findByLiveTrue(Pageable pageable);
+
 }
