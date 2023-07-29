b8fce9e57c3178ca47363b2984d2101f7df18a49
1yogesh123 <yogshelar333@gmail.com>
2023-07-29 19:28:46 +0530

    new commit

---
+++ src/main/java/com/yog/electronicstore/Service/FileService.java
@@ -0,0 +1,19 @@
+package com.yog.electronicstore.Service;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileService {
+    //  public String uploadImage(MultipartFile file,String path) throws IOException;
+    public String uploadFile(MultipartFile file,String path) throws IOException;
+
+    InputStream getResource(String path,String name) throws FileNotFoundException;
+
+
+
+}
+
